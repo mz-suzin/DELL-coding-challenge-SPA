342e1d00679cb553db1d2587ff52402df76e76b1
mz-suzin <mzsuzin@gmail.com>
2023-03-23 10:48:39 -0300

    Working on first section consulta trecho x modalidade, missing half of interface and full functionality

--- script.js
+++ script.js
@@ -7,13 +7,30 @@ import getData from "./scripts/getData.js";
 
 getData().then(data => {
 
-    let dropDownCities = document.getElementById('dropDownCities');
+    // Finds every drop down list with class='dropDownCities' and fills it with the city's name
+    let dropDownCities = document.querySelectorAll('.dropDownCities');
+    dropDownCities.forEach(list => {
+        for (let i = 0; i < data[0].length; i++) {
+            let option = document.createElement('option');
+            option.value = data[0][i];
+            option.innerHTML = data[0][i];
+            list.appendChild(option);
+        }
+    })
 
-    for (let i = 0; i < data[0].length; i++) {
-        let option = document.createElement('option');
-        option.value = data[0][i];
-        option.innerHTML = data[0][i];
-        console.log(data[0][i]);
-        dropDownCities.appendChild(option);
-    }
+    let dropDownTrucks = document.querySelectorAll('.dropDownTrucks');
+    let truckInfo = [
+        'Caminhão Pequeno (1TON Max)',
+        'Caminhão Médio (4TON Max)',
+        'Caminhão Grande (10TON Max'
+    ]
+    dropDownTrucks.forEach(list => {
+        for (let i = 0; i <= 2; i++) {
+            let option = document.createElement('option');
+            option.value = truckInfo[i];
+            option.innerHTML = truckInfo[i];
+            list.appendChild(option);
+        }
+    })
+    
 })
--- scripts/getData.js
+++ scripts/getData.js
@@ -2,45 +2,17 @@
 // March, 2023.
 
 // ********* Data Extraction Section *********
+
 async function getData() {
     try {
+        // Usando fetch para ler os valores do arquivo CSV
+        // O arquivo CSV deve sempre estar junto com o resto dos arquivos do programa.
         const response = await fetch('./scripts/DNIT-Distancias.csv');
         const data = await response.text();
         return Papa.parse(data, { header: false, delimiter: ';'}).data;
     } catch (error) {
-        console.log('error:', error);
+        console.log('error when reading .csv file:', error);
     }
 }
 
 export default getData;
-
-
-// export default function dataExtraction() {
-//     // const fs = require('fs');
-
-//     // Reads the file DNIT-Distancias.csv and returns an array filled with the cities and the distances
-//     // const data = fs.readFileSync('DNIT-Distancias.csv', 'utf8').toString().split(/;|\r\n/);
-
-//     getData().then();
-
-//     return parsedData;
-        
-    
-
-    // const data = d3.csvParse('DNIT-Distancias.csv')
-    // console.log(data);
-
-    // Iterates through all data items and allocates it to cities or distances (converting the distance to type 'number')
-    // const cities = [];
-    // const distances = [];
-
-    // data.forEach(value => {
-    //     if (isNaN(value)) {
-    //         cities.push(value);
-    //     } else {
-    //         distances.push(Number(value));
-    //     }
-    // })
-
-    // return { cities, distances };
-// }
